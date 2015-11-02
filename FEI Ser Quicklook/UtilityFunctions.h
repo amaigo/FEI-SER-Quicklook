9bbfbf1c9b84b0487a4002dd934249f23d1618c1
James LeBeau <jmlebeau@ncsu.edu>
2015-11-02 16:20:51 -0500

    more bug squashing and dramatically reduced memory requirements

--- FEI Ser Quicklook/UtilityFunctions.h
+++ FEI Ser Quicklook/UtilityFunctions.h
@@ -46,6 +46,7 @@ struct SASerHeader {
     int numberDimensions;
 };
 
+CGImageRef GetFirstImageFromURL(CFURLRef emiUrl);
 
 void GetImageParametersWithURL(CFURLRef emiUrl);
 void CreateImageAndDrawEmiImageFromUrl(QLPreviewRequestRef *thePreview, CFURLRef emiUrl, CFDictionaryRef options);
--- FEI Ser Quicklook/UtilityFunctions.m
+++ FEI Ser Quicklook/UtilityFunctions.m
@@ -20,12 +20,14 @@ int *dataOffsetArray;
 
 
 
+
 void GetImageParametersWithURL(CFURLRef emiUrl){
 
 
-    NSData *emiData;
+    NSFileHandle *dataHandle = [NSFileHandle fileHandleForReadingFromURL:(__bridge NSURL * _Nonnull)(emiUrl) error:nil];
+    NSData *emiData = [dataHandle readDataOfLength:10000];
 
-    emiData = [NSData dataWithContentsOfURL:(__bridge NSURL * _Nonnull)(emiUrl)];
+//    emiData = [NSData dataWithContentsOfURL:(__bridge NSURL * _Nonnull)(emiUrl)];
 
     NSRange range;
 
@@ -99,290 +101,241 @@ void CreateImageAndDrawEmiImageFromUrl(QLPreviewRequestRef *thePreview, CFURLRef
 
 
 
+    CGImageRef image = GetFirstImageFromURL(emiUrl);
 
+    
+    //Draw the data
+    CGSize canvasSize;
+    canvasSize.height = CGImageGetHeight(image);
+    canvasSize.width = CGImageGetWidth(image);
+    CGContextRef cgContext = QLPreviewRequestCreateContext(*thePreview, canvasSize, TRUE,  options);
 
-    NSData *emiData;
-    emiData = [NSData dataWithContentsOfURL:(__bridge NSURL * _Nonnull)(emiUrl)];
-    NSRange range;
+    CGAffineTransform flipVertical = CGAffineTransformMake(
+                                                           1, 0, 0, -1, 0, canvasSize.height
+                                                           );
+    CGContextConcatCTM(cgContext, flipVertical);
 
-    if (serHeader.dataTypeId >= 16674) {
+    // Copying context content
+    CGContextDrawImage(cgContext, CGRectMake(0,0, canvasSize.width, canvasSize.height), image);
 
-        range.location =dataOffsetArray[0];
-        range.length = 8;
+    if(serHeader.totalNumberElements>1){
+    // setup the size
 
-        double calibrationOffsetX, calibrationDeltaX; int calibrationElementX;
-        double calibrationOffsetY, calibrationDeltaY; int calibrationElementY;
+        CGRect circleRect;
+        float scale = 1.0;
+        CGContextSetLineWidth(cgContext, 5.0 * scale);
 
-        [emiData getBytes:&calibrationOffsetX range:range ]; //calibration at element calibrationElement along x
-        range.location +=8;
+        CGContextSetFillColorWithColor(cgContext, CGColorCreateGenericGray(1.0, 1.0));
+        float circleSize = canvasSize.width*0.03;
+        float vertPosition = canvasSize.height*0.9;
 
-        [emiData getBytes:&calibrationDeltaX range:range ]; //calibration at element calibrationElement along x
-        range.location +=8;
+        float position;
+        for (int j = -1 ; j<=1; j++) {
+            position = canvasSize.width*(0.5+j*.1);
+           circleRect  = CGRectMake(position, vertPosition, circleSize, circleSize);
 
-        range.length =4;
-        [emiData getBytes:&calibrationElementX range:range ]; //calibration at element calibrationElement along x
-        range.location +=4;
-
-        range.length =8;
-        [emiData getBytes:&calibrationOffsetY range:range ]; //calibration at element calibrationElement along x
-        range.location +=8;
-
-        [emiData getBytes:&calibrationDeltaY range:range ]; //calibration at element calibrationElement along x
-        range.location +=8;
-
-        range.length =4;
-        [emiData getBytes:&calibrationElementY range:range ]; //calibration at element calibrationElement along x
-        range.location +=4;
-
-
-        short dataType;
-
-        range.length =sizeof(short);
-        [emiData getBytes:&dataType range:range]; //calibration at element calibrationElement along x
-        range.location +=sizeof(short);
-
-
-        //        Type = getType(dataType);
-
-        range.length =sizeof(int);
-
-        int arraySizeX, arraySizeY;
-
-        [emiData getBytes:&arraySizeX range:range]; //calibration at element calibrationElement along x
-        range.location +=sizeof(int);
-        [emiData getBytes:&arraySizeY range:range ]; //calibration at element calibrationElement along x
-        range.location +=sizeof(int);
-
-
-
-
-        if (dataType == 2) {
-            int imageSize =arraySizeX*arraySizeY;
-            range.length = imageSize*sizeof(unsigned short);
-
-            unsigned short *image = malloc(range.length);
-            [emiData getBytes:image range:range];
-
-            unsigned short max = 0;
-            unsigned short min = 65535;
-
-            unsigned short temp  = 0;
-            for(int i = 0; i < arraySizeX*arraySizeY; i++){
-
-                temp = image[i];
-
-                if(temp >max)
-                    max = temp;
-                else if(temp < min)
-                    min = temp;
-
-
-            }
-
-            unsigned short maxmin = max - min;
-
-            for(int i = 0; i < arraySizeX*arraySizeY; i++){
-
-                temp = (unsigned short) round(((double) (image[i]-min) / (double) maxmin) * (double) 65536);
-
-                if(temp > 65000)
-                    image[i] = 65000;
-                else if(temp < 0)
-                    image[i] = 0;
-                else
-                    image[i] = temp;
-
-
-
-
-            }
-
-
-            CFDataRef imageData = CFDataCreate(NULL, (UInt8 *) image, range.length);
-
-
-            //Spread the values in the image across the appropriate image range
-            //Contrast-Brightness values should be able to bias this accordingly
-            //
-            //                temp = ((double) (*myPointer-imageParameters.minRange) / (double) rangeDif) * (double) 65536;
-            //
-            //                if(temp > 65000)
-            //                    *myPointer = 65000;
-            //                else if(temp < 0)
-            //                    *myPointer = 0;
-            //                else
-            //                    *myPointer = temp;
-            //
-            //
-            //                myPointer++;
-            //            }
-
-
-
-
-            //Create an image provider and image from the given data
-            CGDataProviderRef imageProvider = CGDataProviderCreateWithCFData(imageData);
-
-            CGImageRef emiImage = CGImageCreate(arraySizeX, arraySizeY, 16, 16, sizeof(short)*arraySizeX, CGColorSpaceCreateWithName(kCGColorSpaceGenericGray), kCGBitmapByteOrder16Little, imageProvider, NULL, TRUE, kCGRenderingIntentDefault);
-
-            //Draw the data
-            CGSize canvasSize;
-            canvasSize.height = arraySizeY;
-            canvasSize.width = arraySizeX;
-            CGContextRef cgContext = QLPreviewRequestCreateContext(*thePreview, canvasSize, TRUE,  options);
-
-            CGAffineTransform flipVertical = CGAffineTransformMake(
-                                                                   1, 0, 0, -1, 0, arraySizeY
-                                                                   );
-            CGContextConcatCTM(cgContext, flipVertical);
-
-            // Copying context content
-            CGContextDrawImage(cgContext, CGRectMake(0,0, arraySizeX, arraySizeY), emiImage);
-
-            if(serHeader.totalNumberElements>1){
-            // setup the size
-
-                CGRect circleRect;
-                float scale = 1.0;
-                CGContextSetLineWidth(cgContext, 5.0 * scale);
-
-                CGContextSetFillColorWithColor(cgContext, CGColorCreateGenericGray(1.0, 1.0));
-                float circleSize = canvasSize.width*0.03;
-                float vertPosition = canvasSize.height*0.9;
-
-                float position;
-                for (int j = -1 ; j<=1; j++) {
-                    position = canvasSize.width*(0.5+j*.1);
-                   circleRect  = CGRectMake(position, vertPosition, circleSize, circleSize);
-
-                    CGContextFillEllipseInRect(cgContext, circleRect);
-                    CGContextStrokeEllipseInRect(cgContext, circleRect);
-
-                }
-
-
-            };
-
-            //Release all the crap from memory, may help to prevent crashes
-            CFRelease(imageData);
-            CGImageRelease(emiImage);
-            CGDataProviderRelease(imageProvider);
-
-            QLPreviewRequestFlushContext(*thePreview, cgContext);
-            CFRelease(cgContext);
-
+            CGContextFillEllipseInRect(cgContext, circleRect);
+            CGContextStrokeEllipseInRect(cgContext, circleRect);
 
         }
 
+
     }
 
+    //Release all the crap from memory, may help to prevent crashes
+    CGImageRelease(image);
+
+    QLPreviewRequestFlushContext(*thePreview, cgContext);
+    CFRelease(cgContext);
 
 
 }
+
 
 void CreateThumbnailFromUrl(QLThumbnailRequestRef *theThumbnail, CFURLRef emiUrl, CFDictionaryRef options){
 
 
-    NSData *emiData;
-    emiData = [NSData dataWithContentsOfURL:(__bridge NSURL * _Nonnull)(emiUrl)];
+    
+    
+    CGImageRef image = GetFirstImageFromURL(emiUrl);
+    
+    
+    //Draw the data
+    CGSize canvasSize;
+    canvasSize.height = CGImageGetHeight(image);
+    canvasSize.width = CGImageGetWidth(image);
+
+              CGContextRef cgContext = QLThumbnailRequestCreateContext(*theThumbnail, canvasSize, TRUE,  options);
+
+    
+    CGAffineTransform flipVertical = CGAffineTransformMake(
+                                                           1, 0, 0, -1, 0, canvasSize.height
+                                                           );
+    CGContextConcatCTM(cgContext, flipVertical);
+    
+    // Copying context content
+    CGContextDrawImage(cgContext, CGRectMake(0,0, canvasSize.width, canvasSize.height), image);
+    
+    if(serHeader.totalNumberElements>1){
+        // setup the size
+        
+        CGRect circleRect;
+        float scale = 1.0;
+        CGContextSetLineWidth(cgContext, 5.0 * scale);
+        
+        CGContextSetFillColorWithColor(cgContext, CGColorCreateGenericGray(1.0, 1.0));
+        float circleSize = canvasSize.width*0.03;
+        float vertPosition = canvasSize.height*0.9;
+        
+        float position;
+        for (int j = -1 ; j<=1; j++) {
+            position = canvasSize.width*(0.5+j*.1);
+            circleRect  = CGRectMake(position, vertPosition, circleSize, circleSize);
+            
+            CGContextFillEllipseInRect(cgContext, circleRect);
+            CGContextStrokeEllipseInRect(cgContext, circleRect);
+            
+        }
+        
+        
+    }
+    
+
+    //Release all the crap from memory, may help to prevent crashes
+
+    CGImageRelease(image);
+
+    QLThumbnailRequestFlushContext(*theThumbnail, cgContext);
+    CFRelease(cgContext);
+
+
+}
+
+
+CGImageRef GetFirstImageFromURL(CFURLRef emiUrl){
+    
+    NSFileHandle *dataHandle = [NSFileHandle fileHandleForReadingFromURL:(__bridge NSURL * _Nonnull)(emiUrl) error:nil];
+    long long offset;
+    offset = dataOffsetArray[0];
+    
+    [dataHandle seekToFileOffset: offset];
+    
+    NSData *emiData = [dataHandle readDataOfLength:400];
+    
+    CGImageRef serImage;
+    
+    
+    //    NSData *emiData;
+    //    emiData = [NSData dataWithContentsOfURL:(__bridge NSURL * _Nonnull)(emiUrl)];
     NSRange range;
-
+    
     if (serHeader.dataTypeId >= 16674) {
-
-        range.location =dataOffsetArray[0];
+        
+        
+        
+        
+        range.location =0;
         range.length = 8;
-
+        
         double calibrationOffsetX, calibrationDeltaX; int calibrationElementX;
         double calibrationOffsetY, calibrationDeltaY; int calibrationElementY;
-
+        
         [emiData getBytes:&calibrationOffsetX range:range ]; //calibration at element calibrationElement along x
         range.location +=8;
-
+        
         [emiData getBytes:&calibrationDeltaX range:range ]; //calibration at element calibrationElement along x
         range.location +=8;
-
+        
         range.length =4;
         [emiData getBytes:&calibrationElementX range:range ]; //calibration at element calibrationElement along x
         range.location +=4;
-
+        
         range.length =8;
         [emiData getBytes:&calibrationOffsetY range:range ]; //calibration at element calibrationElement along x
         range.location +=8;
-
+        
         [emiData getBytes:&calibrationDeltaY range:range ]; //calibration at element calibrationElement along x
         range.location +=8;
-
+        
         range.length =4;
         [emiData getBytes:&calibrationElementY range:range ]; //calibration at element calibrationElement along x
         range.location +=4;
-
-
+        
+        
         short dataType;
-
+        
         range.length =sizeof(short);
         [emiData getBytes:&dataType range:range]; //calibration at element calibrationElement along x
         range.location +=sizeof(short);
-
-
+        
+        
         //        Type = getType(dataType);
-
+        
         range.length =sizeof(int);
-
+        
         int arraySizeX, arraySizeY;
-
+        
         [emiData getBytes:&arraySizeX range:range]; //calibration at element calibrationElement along x
         range.location +=sizeof(int);
         [emiData getBytes:&arraySizeY range:range ]; //calibration at element calibrationElement along x
         range.location +=sizeof(int);
-
-
-
-
+        
+        
+        
+        
         if (dataType == 2) {
+            
+            [dataHandle seekToFileOffset: offset+range.location];
+            
+            range.location=0;
+            
             int imageSize =arraySizeX*arraySizeY;
             range.length = imageSize*sizeof(unsigned short);
-
+            
+            emiData = [dataHandle readDataOfLength:range.length];
+            
+            
             unsigned short *image = malloc(range.length);
             [emiData getBytes:image range:range];
-
+            
             unsigned short max = 0;
             unsigned short min = 65535;
-
+            
             unsigned short temp  = 0;
             for(int i = 0; i < arraySizeX*arraySizeY; i++){
-
+                
                 temp = image[i];
-
+                
                 if(temp >max)
                     max = temp;
                 else if(temp < min)
                     min = temp;
-
-
+                
+                
             }
-
+            
             unsigned short maxmin = max - min;
-
+            
             for(int i = 0; i < arraySizeX*arraySizeY; i++){
-
+                
                 temp = (unsigned short) round(((double) (image[i]-min) / (double) maxmin) * (double) 65536);
-
+                
                 if(temp > 65000)
                     image[i] = 65000;
                 else if(temp < 0)
                     image[i] = 0;
                 else
                     image[i] = temp;
-
-
-
-
+                
+                
+                
+                
             }
-
-
+            
+            
             CFDataRef imageData = CFDataCreate(NULL, (UInt8 *) image, range.length);
-
-
+            
+            
             //Spread the values in the image across the appropriate image range
             //Contrast-Brightness values should be able to bias this accordingly
             //
@@ -398,67 +351,24 @@ void CreateThumbnailFromUrl(QLThumbnailRequestRef *theThumbnail, CFURLRef emiUrl
             //
             //                myPointer++;
             //            }
-
-
-
-
+            
+            
+            
+            
             //Create an image provider and image from the given data
             CGDataProviderRef imageProvider = CGDataProviderCreateWithCFData(imageData);
-
-            CGImageRef emiImage = CGImageCreate(arraySizeX, arraySizeY, 16, 16, sizeof(short)*arraySizeX, CGColorSpaceCreateWithName(kCGColorSpaceGenericGray), kCGBitmapByteOrder16Little, imageProvider, NULL, TRUE, kCGRenderingIntentDefault);
-
-            //Draw the data
-            CGSize canvasSize;
-            canvasSize.height = arraySizeY;
-            canvasSize.width = arraySizeX;
-            CGContextRef cgContext = QLThumbnailRequestCreateContext(*theThumbnail, canvasSize, TRUE,  options);
-
-            CGAffineTransform flipVertical = CGAffineTransformMake(
-                                                                   1, 0, 0, -1, 0, arraySizeY
-                                                                   );
-            CGContextConcatCTM(cgContext, flipVertical);
-
-            // Copying context content
-            CGContextDrawImage(cgContext, CGRectMake(0,0, arraySizeX, arraySizeY), emiImage);
-
-            if(serHeader.totalNumberElements>1){
-                // setup the size
-
-                CGRect circleRect;
-                float scale = 1.0;
-                CGContextSetLineWidth(cgContext, 5.0 * scale);
-
-                CGContextSetFillColorWithColor(cgContext, CGColorCreateGenericGray(1.0, 1.0));
-                float circleSize = canvasSize.width*0.03;
-                float vertPosition = canvasSize.height*0.9;
-
-                float position;
-                for (int j = -1 ; j<=1; j++) {
-                    position = canvasSize.width*(0.5+j*.1);
-                    circleRect  = CGRectMake(position, vertPosition, circleSize, circleSize);
-
-                    CGContextFillEllipseInRect(cgContext, circleRect);
-                    CGContextStrokeEllipseInRect(cgContext, circleRect);
-
-                }
-
-
-            };
-
-
-            //Release all the crap from memory, may help to prevent crashes
+            
+            serImage = CGImageCreate(arraySizeX, arraySizeY, 16, 16, sizeof(short)*arraySizeX, CGColorSpaceCreateWithName(kCGColorSpaceGenericGray), kCGBitmapByteOrder16Little, imageProvider, NULL, TRUE, kCGRenderingIntentDefault);
+            
             CFRelease(imageData);
-
-            CGImageRelease(emiImage);
             CGDataProviderRelease(imageProvider);
 
-            QLThumbnailRequestFlushContext(*theThumbnail, cgContext);
-            CFRelease(cgContext);
 
-
+            
+            
         }
-
     }
-
-
+    
+    return serImage;
 }
+
